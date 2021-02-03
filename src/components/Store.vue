1f2bc228f3204e90dd0d709e4bca7d5408a7bedc
byluni <ililuc17@htl-kaindorf.at>
2021-02-03 20:36:06 +0100

    geändert:       src/components/Store.vue gelöscht:       src/components/StoreApp.vue geändert:       src/plugins/bootstrap-vue.js

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>
-    <div v-for="app in this.apps.value" :key="app">
-      {{app.group_name}}
-      <div v-for="appname in apps.value" :key="appname">
-        {{appname.app_name}}
+  <div class="container">
+    <div v-for="app in this.apps.value" :key="app" style="padding-top: 10px">
+      <div class="text-center">
+        <h3>{{app.group_name}}</h3>
       </div>
+
+      <div class="row">
+        <div class="col-lg-3" style="padding-top: 10px" v-for="appname in app.group_members" :key="appname">
+        
+        <div class="card" style="height: 400px">
+          <div class="card-header text-center"><img v-bind:src="appname.icon_url" style="width: 100px; height: 100px"></div>
+          <div class="card-body"><b>{{appname.app_dispay_name}}</b><br>
+                                    {{appname.short_description}}
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <!-- bootstrap-vue.org/docs/icons -->
+              <div class="col-sm text-center">
+                <button><b-icon icon="info-circle"></b-icon></button>
+                <button><b-icon icon="gear-fill"></b-icon></button>
+                <button><b-icon icon="power"></b-icon></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
+      
     </div>
   </div>
 </template>
